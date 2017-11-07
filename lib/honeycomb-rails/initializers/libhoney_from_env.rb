bd47d3c1ebcc08132d4aa35cdc066f15c87d2f70
Sam Stokes <me@samstokes.co.uk>
2017-11-07 10:34:41 -0600

    Slightly nicen error message

--- lib/honeycomb-rails/initializers/libhoney_from_env.rb
+++ lib/honeycomb-rails/initializers/libhoney_from_env.rb
@@ -4,7 +4,8 @@ module HoneycombRails
   module Initializers
     module LibhoneyFromEnv
       def set_libhoney_from_env!
-        @libhoney = Libhoney::Client.new(writekey: ENV.fetch('HONEYCOMB_WRITEKEY'),
+        writekey = ENV['HONEYCOMB_WRITEKEY'] or raise LoadError, 'boom' # TODO
+        @libhoney = Libhoney::Client.new(writekey: writekey,
                                         dataset:  ENV['HONEYCOMB_DATASET'] || 'rails')
       end
 
