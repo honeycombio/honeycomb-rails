34ca92a2d65a5a01c33dd41636b228623888cb4c
Christine Yen <cyen@christineyen.com>
2018-01-22 18:59:53 -0800

    Lazily initialize @honeycomb_metadata instead of relying on before_action hook

--- lib/honeycomb-rails/extensions/action_controller.rb
+++ lib/honeycomb-rails/extensions/action_controller.rb
@@ -2,18 +2,6 @@ module HoneycombRails
   module Extensions
     module ActionController
       module InstanceMethods
-        def self.included(controller_class)
-          super
-
-          controller_class.before_action do
-            honeycomb_initialize
-          end
-        end
-
-        def honeycomb_initialize
-          @honeycomb_metadata = {}
-        end
-
         # Hash of metadata to be added to the event we will send to Honeycomb
         # for the current request.
         #
@@ -23,7 +11,9 @@ module HoneycombRails
         #     honeycomb_metadata[:num_posts] = @posts.size
         #
         # @return [Hash<String=>Any>]
-        attr_reader :honeycomb_metadata
+        def honeycomb_metadata
+          @honeycomb_metadata ||= {}
+        end
       end
     end
   end
